5f5e5348813bf634e3206b9bddec557a9361a350
mohamedarifhussain <mohamedarifhussain786@gmail.com>
2023-10-30 21:48:51 +0530

    My projects 9th updated

--- project_9_file_organizer/file_organizer_program.py
+++ project_9_file_organizer/file_organizer_program.py
@@ -1,14 +1,34 @@
 import os
+
 print("Welcome to file organizer!\n")
+
 print("""You can organize different types of files 
 into required folders\n""")
-file_path = input('Enter the file path: ')
-os.chdir(file_path)
-print(os.getcwd())
-print(os.listdir())
 
-# files = os.listdir()
-# print(files)
+file_path = input('Enter the file path: ')
+
+os.chdir(file_path)
+
+print(f"current working directory: {os.getcwd()}")
+
+files_of_organizer = os.listdir(r"C:\Users\USER\Documents\Python Scripts\PythonProjects\project_9_file_organizer\file organizer")
+
+files = os.listdir()
+print(f"Current working directory's folders: {files}")
+print(f"files_of_organizer (files): {files_of_organizer}")
+
+
+# #TODO-1: The below code is a new method to for organize file.
+
+for file in files:
+    type_of_file = file[file.index('.')+1:]
+    if type_of_file not in files_of_organizer:
+        os.makedirs(f"C:\\Users\\USER\Documents\\Python Scripts\\PythonProjects\\project_9_file_organizer\\file organizer\\" + type_of_file)
+    destination = "C:\\Users\\USER\\Documents\\Python Scripts\\PythonProjects\\project_9_file_organizer\\file organizer\\" + type_of_file
+
+    os.rename(src=file_path+file, dst=destination+'\\'+file)
+
+# #TODO-2: The below code is a different method to organize file so ignore it.
 # for i in files:
 #     if '.' in i:
 #         index = i.index('.')
@@ -27,5 +47,3 @@ print(os.listdir())
 #
 #
 # # os.rename(r"C:\Users\USER\Documents\file organizer\SAMPLE.txt", r"C:\Users\USER\Pictures\SAMPLE.txt")
-#
-# print(os.listdir())
